54a36067502e68340cf68a3be90a0dbdb5b0cd7c
Gökberk Karaca <gokberk.karaca@outlook.com>
2018-11-05 01:48:33 +0300

    Create data folder

--- newsgroup_classifier.py
+++ newsgroup_classifier.py
@@ -1,19 +1,19 @@
 training_feature_vectors = []
-with open('question-4-train-features.csv') as f:
+with open('data/question-4-train-features.csv') as f:
     for line in f.read().splitlines():
         training_feature_vectors.append(line.split(','))
 
 training_label_vectors = []
-with open('question-4-train-labels.csv') as f:
+with open('data/question-4-train-labels.csv') as f:
     for line in f.read().splitlines():
         training_label_vectors.append(line)
 
 test_feature_vectors = []
-with open('question-4-test-features.csv') as f:
+with open('data/question-4-test-features.csv') as f:
     for line in f.read().splitlines():
         test_feature_vectors.append(line.split(','))
 
 test_label_vectors = []
-with open('question-4-test-labels.csv') as f:
+with open('data/question-4-test-labels.csv') as f:
     for line in f.read().splitlines():
         test_label_vectors.append(line)
